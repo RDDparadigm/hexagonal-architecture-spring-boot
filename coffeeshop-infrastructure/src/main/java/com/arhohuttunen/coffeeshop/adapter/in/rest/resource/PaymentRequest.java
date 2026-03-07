a6c3191b17711b4b7b1b5f4702be9c359281610d
Arho Huttunen <6802462+arhohuttunen@users.noreply.github.com>
2026-03-07 15:18:11 +0200

    Remove constraints for clarity
    We are not testing this at all in the examples.

--- coffeeshop-infrastructure/src/main/java/com/arhohuttunen/coffeeshop/adapter/in/rest/resource/PaymentRequest.java
+++ coffeeshop-infrastructure/src/main/java/com/arhohuttunen/coffeeshop/adapter/in/rest/resource/PaymentRequest.java
@@ -1,11 +1,9 @@
 package com.arhohuttunen.coffeeshop.adapter.in.rest.resource;
 
-import jakarta.validation.constraints.NotNull;
-
 import java.time.YearMonth;
 
 public record PaymentRequest(
-        @NotNull String cardHolderName,
-        @NotNull String cardNumber,
-        @NotNull YearMonth expiry) {
+        String cardHolderName,
+        String cardNumber,
+        YearMonth expiry) {
 }
