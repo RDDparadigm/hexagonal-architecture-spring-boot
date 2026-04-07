54b76795dac5a8a3f3311211b78f429176d2f432
RDDParadigm <matteo.inchingolo97@gmail.com>
2026-04-07 21:35:04 +0200

    Fix for Spring validation

--- coffeeshop-infrastructure/src/main/java/com/arhohuttunen/coffeeshop/adapter/in/rest/resource/LineItemRequest.java
+++ coffeeshop-infrastructure/src/main/java/com/arhohuttunen/coffeeshop/adapter/in/rest/resource/LineItemRequest.java
@@ -5,10 +5,10 @@ import com.arhohuttunen.coffeeshop.shared.Drink;
 import com.arhohuttunen.coffeeshop.shared.Milk;
 import com.arhohuttunen.coffeeshop.shared.Size;
 
-import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Positive;
 
-public record LineItemRequest(@NotNull Drink drink, @NotEmpty Milk milk, @NotEmpty Size size, @Positive Integer quantity) {
+public record LineItemRequest(@NotNull Drink drink, @NotNull Milk milk, @NotNull Size size, @Positive @NotNull Integer quantity) {
     public LineItem toDomain() {
         return new LineItem(drink, milk, size, quantity);
     }
--- coffeeshop-infrastructure/src/main/java/com/arhohuttunen/coffeeshop/adapter/in/rest/resource/OrderRequest.java
+++ coffeeshop-infrastructure/src/main/java/com/arhohuttunen/coffeeshop/adapter/in/rest/resource/OrderRequest.java
@@ -3,12 +3,13 @@ package com.arhohuttunen.coffeeshop.adapter.in.rest.resource;
 import com.arhohuttunen.coffeeshop.application.order.LineItem;
 import com.arhohuttunen.coffeeshop.shared.Location;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 
 import java.util.List;
 
-public record OrderRequest(@NotNull Location location, @NotEmpty List<LineItemRequest> items) {
+public record OrderRequest(@NotNull Location location, @NotEmpty @Valid List<LineItemRequest> items) {
     public List<LineItem> toDomainItems() {
         return items.stream().map(LineItemRequest::toDomain).toList();
     }
